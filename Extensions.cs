21bec52d8571b6d81129e073dcc5c01f2f49cbc9
Eric Blanchard <Eric.x2@gmail.com>
2018-03-12 23:41:12 -0400

    First commit, moving over to submodule system

---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Extensions {
+
+    #region Transform Extensions
+    public static void SetX(this Transform tran, float x)
+    {
+        tran.position = new Vector3(x, tran.position.y, tran.position.z);
+    }
+    public static void SetY(this Transform tran, float y)
+    {
+        tran.position = new Vector3(tran.position.x, y, tran.position.z);
+    }
+    public static void SetZ(this Transform tran, float z)
+    {
+        tran.position = new Vector3(tran.position.x, tran.position.y, z);
+    }
+    #endregion
+}
